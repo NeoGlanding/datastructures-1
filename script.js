83dc029259ed9f9942fc3e57d558a773e7f709f7
NeoGlanding <neoglanding@protonmail.com>
2021-08-03 21:03:21 +0700

    constant big o

--- script.js
+++ script.js
@@ -8,10 +8,18 @@ function findNemo(arr) {
         if (arr[i] === 'nemo') console.log('I found nemo')
     }
     let t1 = performance.now();
-    console.log(`This function is run ${t1-t0} miliseconds`)
+    console.log(`This function is run ${t1-t0} miliseconds`);
 }
 
-findNemo(arr)
+// findNemo(arr)
+// BIG O => O(n) => Operasi looping tergantung banyaknya elemen pada Array. (Linear) => Good
 
-// BIG O => O(n) => Operasi looping tergantung banyaknya elemen pada Array.
+const boxes = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17];
 
+function compression(boxes) {
+    console.log(boxes[0]); // O(1);
+    console.log(boxes[1]) // O(1);
+}
+
+compression(boxes)
+// BIG O => O(2) => Operasi looping ini konstan atau tidak bergantung kepada banyak input (Constant)  => Excellent
